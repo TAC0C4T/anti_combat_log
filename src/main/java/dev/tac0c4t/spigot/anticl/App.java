d3f20eb6e4250f7ab1bb45f01c138ffaf469df09
Carter R <tac0c4t4life@gmail.com>
2024-04-13 18:54:58 -0700

    leaving

--- src/main/java/dev/tac0c4t/spigot/anticl/App.java
+++ src/main/java/dev/tac0c4t/spigot/anticl/App.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -19,22 +20,37 @@ import java.util.HashMap;
 public class App extends JavaPlugin
 {
     private final Map<Entity, Entity> battles = new HashMap<>();
-   
+    private final Map<Player, Boolean> sentenced = new HashMap<>();
     @Override
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(new CombatListener(this), this);   
     }
 
     public void startCombat(Entity attacker, Entity defender) {
+        /*if(!(attacker instanceof Player) || !(defender instanceof Player)) {
+            return;
+        }*/
         if (battles.containsKey(attacker) || battles.containsKey(defender)) {
             return;
         }
+
         Bukkit.broadcastMessage("combat begin");
         battles.put(attacker, defender);
         battles.put(defender, attacker);
+
         Bukkit.getScheduler().runTaskLater(this, () -> {
-            checkCombatParticipants();
+            endCombat(attacker);
+            endCombat(defender);
         }, 5 * 20);
+    }
+
+    public void endCombat(Entity entity) {
+        Entity opponent = battles.remove(entity);
+        if (opponent != null) {
+            if (opponent instanceof Player && ((Player) opponent).isOnline()) {
+                ((Player) opponent).sendMessage("combat over");
+            }
+        }
     }
 
     public void checkCombatParticipants() {
@@ -43,5 +59,21 @@ public class App extends JavaPlugin
                 Bukkit.broadcastMessage(entity.getName() + "did the cringe");
             }
         }
+    }
+
+    public boolean isInCombat(Entity entity) {
+        return battles.containsKey(entity);
+    }
+
+    public void sentence(Player player) {
+        sentenced.put(player, true);
+    }
+
+    public boolean isSentenced(Player player) {
+        return sentenced.getOrDefault(player, false);
+    }
+
+    public void pardon(Player player) {
+        sentenced.remove(player);
     }
 }
--- src/main/java/dev/tac0c4t/spigot/anticl/CombatListener.java
+++ src/main/java/dev/tac0c4t/spigot/anticl/CombatListener.java
@@ -5,6 +5,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.entity.Entity;
 import org.bukkit.Bukkit;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.entity.Player;;
 
 public class CombatListener implements Listener{
     private final App plugin;
@@ -22,4 +26,37 @@ public class CombatListener implements Listener{
 
     }
 
+    @EventHandler
+    public void onPlayerDeath(EntityDeathEvent event){
+         Entity player = event.getEntity();
+         if (plugin.isInCombat(player)) {
+            plugin.endCombat(player);
+            Bukkit.broadcastMessage("has died in combat!");
+         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        
+        if (plugin.isInCombat(player)) {
+            Bukkit.broadcastMessage(player.getName() + " has left the game in combat!");
+            plugin.sentence(player);
+        }
+
+        plugin.endCombat(player);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Bukkit.broadcastMessage("TEST");
+        System.out.println(plugin.isSentenced(player));
+        if (plugin.isSentenced(player)) {
+            //Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "kill" + " " + player.getName());
+            Bukkit.broadcastMessage("has rejoined after combat logging");
+            System.out.println("here");
+            plugin.pardon(player);
+        }
+    }
 }
